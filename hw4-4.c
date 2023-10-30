8921d459f8c7409ae24bbb61891c4c9672f60f7b
andy7560683 <34652047+andy7560683@users.noreply.github.com>
2023-10-31 01:27:21 +0800

    Update hw4-4.c

--- hw4-4.c
+++ hw4-4.c
@@ -1,56 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    while(1){
-        if(1 <= n & n <= 10){
-            break;
+int main() {
+    int s1, d1, s2, d2, s3, d3;
+    scanf("%d %d %d %d %d %d", &s1, &d1, &s2, &d2, &s3, &d3);
+    int a[25] = {0};
+    for (int i = s1; i < d1; i++) {
+        a[i]++;
+    }
+    for (int i = s2; i < d2; i++) {
+        a[i]++;
+    }
+    for (int i = s3; i < d3; i++) {
+        a[i]++;
+    }
+    int ans = 0;
+    for (int i = 0; i < 25; i++) {
+        if (a[i] > ans) {
+            ans = a[i];
         }
     }
+    printf("%d\n", ans);
 
+    int c[3][2]={{s1,d1},{s2,d2},{s3,d3}};
+    int d[3][2];
+    
+for(int k = 0; k < 3; k++){
 
-    int b = n;
-    int g = n;
-    int a[b][g];
-    int m;
-    for(int i = 0; i < n; i++){
-        for(int s = 0; s < n; s++){
-            scanf("%d", &m);
-            a[i][s] = m;
+    int min = 0;
+
+    for(int i = 0; i < 3; i++){
+        if(c[i][0] < min && c[i][0] != 0){
+            min = c[i][0];
+            d[i][0] = min;
         }
     }
-
-for(int k = 0; k < n; k++){
-
-    int max = 0;
-    int max_X, max_Y;
-    int x, y;
-
-    for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(a[i][j] > max){
-                   max = a[i][j];
-                   max_X = i;
-                   max_Y = j;
-                }
-            }
+    for(int i = 0; i < 3; i++){
+        if(min == c[i][0]){
+            c[i][0] = 0;
+        }
     }
-
-    for(int i = 0; i < n; i++){
-        a[max_X][i] = 0;
-        a[i][max_Y] = 0;
+    min = 0;
+    for(int j = 0; j < 3; j++){
+        if(c[j][1] < min && c[j][1] != 0){
+            min = c[j][1];
+            d[j][1] = min;
+        }
     }
-
-    printf("boy %d pair with girl %d\n", max_X+1, max_Y+1);
+    for(int i = 0; i < 3; i++){
+        if(min == c[i][1]){
+            c[i][1] = 0;
+        }
+    }
 }
-return 0;
+    if(ans == 1){
+      printf("Driver 1's schedule is %d %d %d %d %d %d\n", d[0][0], d[0][1], d[1][0], d[1][1], d[2][0], d[2][1]);
+    }
+    else if(ans == 2 && d[0][1] <= d[1][0] && d[1][1] > d[2][0]){
+      printf("Driver 1's schedule is %d %d %d %d\n", d[0][0], d[0][1], d[1][0], d[1][1]);
+      printf("Driver 2's schedule is %d %d\n", d[2][0], d[2][1]);
+    }
+    else if(ans == 2 && d[1][1] <= d[2][0] && d[0][1] > d[1][0]){
+      printf("Driver 1's schedule is %d %d\n", d[0][0], d[0][1]);
+      printf("Driver 2's schedule is %d %d %d %d\n", d[1][0], d[1][1], d[2][0], d[2][1]);
+    }
+    else if(ans == 3){
+      printf("Driver 1's schedule is %d %d\n",d[0][0], d[0][1]);
+      printf("Driver 2's schedule is %d %d\n",d[1][0], d[1][1]);
+      printf("Driver 3's schedule is %d %d\n",d[2][0], d[2][1]);
+    }
+    return 0;
 }
-
-
-
-//boy x pair with girl y
-
-
+//Driver 1's schedule is
